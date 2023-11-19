ebbc863a885c0013e4aebdaf096c15e98cac4f0d
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 21:25:53 +0200

    Added class BookShop

---
+++ BookShop/BookShop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    public class BookShop: IBookOwner, IBookSeller
+    {
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public decimal ProfitFactor { get; private set; } = 1.2M;
+        public Distributor Distributor { get; private set; }
+        public List<Customer> Customers { get; private set; }
+        public decimal Balance { get; private set; }
+        public List<ProtoBook> OwnedLiterature { get; set; }
+
+        public BookShop(string name, string address, string phone, decimal profitFactor, Distributor distributor, decimal StartBalance)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+            ProfitFactor = profitFactor;
+            Distributor = distributor;
+            Balance = StartBalance;
+            OwnedLiterature = new List<ProtoBook>();
+        }
+
+        public void Sell(ProtoBook Item, IBookOwner NewOwner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
