3c5391dc2ff17e2e1371ad874ded88ecf08243c2
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 20:18:43 +0200

    Added Genre enum

---
+++ BookShop/Genre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    public enum Genre
+    {
+        Adventure,
+        Humour,
+        Horror,
+        Detective,
+        FairyTale,
+        Fantasy,
+        Mythology,
+        Romance,
+        SciFi,
+        Thriller,
+        Education
+    }
+}
