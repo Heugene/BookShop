482eb2ed84b7abbac869bb06cd7fe1c189a3742a
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 20:54:25 +0200

    Added abstract class ProtoBook

---
+++ BookShop/ProtoBook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    public abstract class ProtoBook
+    {
+        public static string LitType;
+        public string Name {  get; private set; }
+        public string Author { get; private set; }
+        public int YearPublished { get; private set; }
+        public string ISBN { get; private set; }
+        public int PagesCount { get; private set; }
+        public bool Colourful { get; private set; }
+        public decimal Price { get; private set; }
+
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeOwner(IBookOwner bookOwner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Info()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProtoBook(string name, string author, int yearPublished, string isbn, int pagesCount, bool Colourful, decimal price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProtoBook(ProtoBook Clone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
