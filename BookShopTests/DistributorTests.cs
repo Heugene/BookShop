889ac32783958d10873b8088e0eb56254bd2919b
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 23:09:14 +0200

    Added tests for Distributor class

---
+++ BookShopTests/DistributorTests.cs
@@ -0,0 +1,26 @@
+﻿using BookShop;
+using System.Net;
+
+namespace BookShopTests
+{
+    [TestClass]
+    public class DistributorTests
+    {
+        [TestMethod]
+        public void Sell_Valid()
+        {
+            //arrange
+            int initialBalance = 10000;
+            Comix comix = new Comix("TestTitle", "TestAuthor", 2012, "978-3-16-148410-0", 342, true, 199.99M);
+            Distributor distributor = new Distributor("aboba", "amogus", "92380483084034");
+            BookShop.BookShop shop = new BookShop.BookShop("aboba", "amogus", "92380483084034", 1.2M, distributor, initialBalance);
+            comix.ChangeOwner(distributor);
+
+            //act
+            distributor.Sell(comix, shop);
+
+            //assert
+            Assert.IsTrue(!distributor.OwnedLiterature.Contains(comix) && shop.OwnedLiterature.Contains(comix) && shop.Balance == initialBalance - comix.Price);
+        }
+    }
+}
