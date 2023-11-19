b8604b8323538fdee2074a2b505fabb0a8a24220
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 23:23:44 +0200

    Added tests for BookShop class

---
+++ BookShopTests/BookShopTests.cs
@@ -0,0 +1,61 @@
+﻿using BookShop;
+using System.Net;
+
+namespace BookShopTests
+{
+    [TestClass]
+    public class BookShopTests
+    {
+        [TestMethod]
+        public void Sell_Valid()
+        {
+            //arrange
+            int initialBalance = 10000;
+            Artbook artbook = new Artbook("TestTitle", "TestAuthor", 2012, "978-3-16-148410-0", 342, true, 199.99M);
+            Distributor distributor = new Distributor("aboba", "amogus", "92380483084034");
+            BookShop.BookShop shop = new BookShop.BookShop("aboba", "amogus", "92380483084034", 1.2M, distributor, initialBalance);
+            Customer customer = new Customer("Petro");
+            artbook.ChangeOwner(shop);
+
+            //act
+            shop.Sell(artbook, customer);
+
+            //assert
+            Assert.IsTrue(!shop.OwnedLiterature.Contains(artbook) && customer.OwnedLiterature.Contains(artbook) && shop.Balance == initialBalance + artbook.Price * shop.ProfitFactor);
+        }
+
+        [TestMethod]
+        public void OrderBook_Success()
+        {
+            //arrange
+            int initialBalance = 10000;
+            Artbook artbook = new Artbook("TestTitle", "TestAuthor", 2012, "978-3-16-148410-0", 342, true, 199.99M);
+            Distributor distributor = new Distributor("aboba", "amogus", "92380483084034");
+            BookShop.BookShop shop = new BookShop.BookShop("aboba", "amogus", "92380483084034", 1.2M, distributor, initialBalance);
+            artbook.ChangeOwner(distributor);
+
+            //act
+            bool result = shop.OrderBook(artbook, 10);
+
+            //assert
+            Assert.IsTrue(result && shop.OwnedLiterature.FindAll(X => X.ISBN == "978-3-16-148410-0").Count == 10);
+        }
+
+        [TestMethod]
+        public void OrderBook_Fail()
+        {
+            //arrange
+            int initialBalance = 1000;
+            Book book = new Book("TestTitle", "TestAuthor", 2012, "978-3-16-148410-0", 342, true, 199.99M);
+            Distributor distributor = new Distributor("aboba", "amogus", "92380483084034");
+            BookShop.BookShop shop = new BookShop.BookShop("aboba", "amogus", "92380483084034", 1.2M, distributor, initialBalance);
+            book.ChangeOwner(distributor);
+
+            //act
+            bool result = shop.OrderBook(book, 10);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+}
