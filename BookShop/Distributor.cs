33f08891028fe8c0cf4f7a7c6d02690c7655fabb
Yevhenii Heoka@git config --global user.name Yevhenii <109804875+Heugene@users.noreply.github.com>
2023-11-19 21:30:27 +0200

    Added class Distributor

---
+++ BookShop/Distributor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop
+{
+    public class Distributor: IBookOwner, IBookSeller
+    {
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public decimal Balance { get; private set; } = 0;
+        public List<ProtoBook> OwnedLiterature { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Distributor(string name, string address, string phone)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+
+        public void Sell(ProtoBook Item, IBookOwner NewOwner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
